11edaa609732b00f869bdd2664c9b8bbb0a98cac
Jeremy Satterfield <jsatt22@gmail.com>
2014-03-05 11:09:18 -0600

    don't worry about order of flags and improve mocking in tests

--- affect/tests/middleware_tests.py
+++ affect/tests/middleware_tests.py
@@ -21,14 +21,14 @@ class AffectMiddlewareRequestTest(TestCase):
         self.mw = AffectMiddleware()
         self.mock = mox.Mox()
         self.mock.StubOutWithMock(middleware, 'meets_criteria')
+        self.mock.StubOutWithMock(cache, 'get')
+        self.mock.StubOutWithMock(cache, 'add')
 
     def tearDown(self):
         self.mock.UnsetStubs()
 
     def test_criteria_active_nothing_cached(self):
         self.mock.StubOutWithMock(middleware, 'cache_criteria')
-        self.mock.StubOutWithMock(cache, 'get')
-        self.mock.StubOutWithMock(cache, 'add')
         cache.get('criteria:all')
         cache.add('criteria:all', mox.SameElementsAs(Criteria.objects.all()))
         middleware.meets_criteria(self.request, self.criteria).AndReturn(True)
@@ -49,8 +49,6 @@ class AffectMiddlewareRequestTest(TestCase):
 
     def test_criteria_active_everything_cached(self):
         self.mock.StubOutWithMock(middleware, 'cache_criteria')
-        self.mock.StubOutWithMock(cache, 'get')
-        self.mock.StubOutWithMock(cache, 'add')
         cache.get('criteria:all').AndReturn(Criteria.objects.all())
         middleware.meets_criteria(self.request, self.criteria).AndReturn(True)
         cache.get('criteria:test_crit:flags').AndReturn(
@@ -63,11 +61,13 @@ class AffectMiddlewareRequestTest(TestCase):
         self.mock.VerifyAll()
 
         self.assertDictEqual(self.request.affected_persist, {})
-        self.assertListEqual(self.request.affected_flags,
+        self.assertItemsEqual(self.request.affected_flags,
                              [self.flag1.name, self.flag2.name])
 
     def test_criteria_not_active(self):
         middleware.meets_criteria(self.request, self.criteria).AndReturn(False)
+        cache.get('criteria:all')
+        cache.add('criteria:all', mox.IgnoreArg())
 
         self.mock.ReplayAll()
         self.mw.process_request(self.request)
@@ -80,6 +80,17 @@ class AffectMiddlewareRequestTest(TestCase):
         self.criteria.persistent = True
         self.criteria.save()
         middleware.meets_criteria(self.request, self.criteria).AndReturn(True)
+        cache.get('criteria:all')
+        cache.add('criteria:all', mox.IgnoreArg())
+        cache.get('criteria:test_crit:flags')
+        cache.add('criteria:test_crit', mox.IgnoreArg())
+        cache.add('criteria:test_crit:flags', mox.IgnoreArg())
+        cache.add('criteria:test_crit:users', mox.IgnoreArg())
+        cache.add('criteria:test_crit:groups', mox.IgnoreArg())
+        cache.get('flag_conflicts:other_flag').InAnyOrder()
+        cache.get('flag_conflicts:test_flag').InAnyOrder()
+        cache.add('flag_conflicts:test_flag', mox.IgnoreArg()).InAnyOrder()
+        cache.add('flag_conflicts:other_flag', mox.IgnoreArg()).InAnyOrder()
 
         self.mock.ReplayAll()
         self.mw.process_request(self.request)
@@ -87,13 +98,24 @@ class AffectMiddlewareRequestTest(TestCase):
 
         self.assertDictEqual(
             self.request.affected_persist, {self.criteria: True})
-        self.assertListEqual(
+        self.assertItemsEqual(
             self.request.affected_flags, [self.flag1.name, self.flag2.name])
 
     def test_persistent_w_cookie(self):
         self.request.COOKIES['dac_test_crit'] = 'True'
         self.criteria.persistent = True
         self.criteria.save()
+        cache.get('criteria:all')
+        cache.add('criteria:all', mox.IgnoreArg())
+        cache.get('criteria:test_crit:flags')
+        cache.add('criteria:test_crit', mox.IgnoreArg())
+        cache.add('criteria:test_crit:flags', mox.IgnoreArg())
+        cache.add('criteria:test_crit:users', mox.IgnoreArg())
+        cache.add('criteria:test_crit:groups', mox.IgnoreArg())
+        cache.get('flag_conflicts:other_flag').InAnyOrder()
+        cache.get('flag_conflicts:test_flag').InAnyOrder()
+        cache.add('flag_conflicts:test_flag', mox.IgnoreArg()).InAnyOrder()
+        cache.add('flag_conflicts:other_flag', mox.IgnoreArg()).InAnyOrder()
 
         self.mock.ReplayAll()
         self.mw.process_request(self.request)
@@ -101,7 +123,7 @@ class AffectMiddlewareRequestTest(TestCase):
 
         self.assertDictEqual(
             self.request.affected_persist, {self.criteria: True})
-        self.assertListEqual(
+        self.assertItemsEqual(
             self.request.affected_flags, [self.flag1.name, self.flag2.name])
 
     def test_flag_conflicts(self):
@@ -109,6 +131,17 @@ class AffectMiddlewareRequestTest(TestCase):
         self.flag2.conflicts.add(self.flag1)
         self.flag2.priority = 100
         self.flag2.save()
+        cache.get('criteria:all')
+        cache.add('criteria:all', mox.IgnoreArg())
+        cache.get('criteria:test_crit:flags')
+        cache.add('criteria:test_crit', mox.IgnoreArg())
+        cache.add('criteria:test_crit:flags', mox.IgnoreArg())
+        cache.add('criteria:test_crit:users', mox.IgnoreArg())
+        cache.add('criteria:test_crit:groups', mox.IgnoreArg())
+        cache.get('flag_conflicts:other_flag').InAnyOrder()
+        cache.get('flag_conflicts:test_flag').InAnyOrder()
+        cache.add('flag_conflicts:test_flag', mox.IgnoreArg()).InAnyOrder()
+        cache.add('flag_conflicts:other_flag', mox.IgnoreArg()).InAnyOrder()
 
         self.mock.ReplayAll()
         self.mw.process_request(self.request)
@@ -116,13 +149,24 @@ class AffectMiddlewareRequestTest(TestCase):
 
         self.assertDictEqual(
             self.request.affected_persist, {})
-        self.assertListEqual(
+        self.assertItemsEqual(
             self.request.affected_flags, [self.flag2.name])
 
     def test_flag_conflict_not_in_criteria(self):
         middleware.meets_criteria(self.request, self.criteria).AndReturn(True)
         flag3 = Flag.objects.create(name='that_flag', priority=100)
         flag3.conflicts.add(self.flag1, self.flag2)
+        cache.get('criteria:all')
+        cache.add('criteria:all', mox.IgnoreArg())
+        cache.get('criteria:test_crit:flags')
+        cache.add('criteria:test_crit', mox.IgnoreArg())
+        cache.add('criteria:test_crit:flags', mox.IgnoreArg())
+        cache.add('criteria:test_crit:users', mox.IgnoreArg())
+        cache.add('criteria:test_crit:groups', mox.IgnoreArg())
+        cache.get('flag_conflicts:other_flag').InAnyOrder()
+        cache.get('flag_conflicts:test_flag').InAnyOrder()
+        cache.add('flag_conflicts:test_flag', mox.IgnoreArg()).InAnyOrder()
+        cache.add('flag_conflicts:other_flag', mox.IgnoreArg()).InAnyOrder()
 
         self.mock.ReplayAll()
         self.mw.process_request(self.request)
@@ -130,7 +174,7 @@ class AffectMiddlewareRequestTest(TestCase):
 
         self.assertDictEqual(
             self.request.affected_persist, {})
-        self.assertListEqual(
+        self.assertItemsEqual(
             self.request.affected_flags, [self.flag1.name, self.flag2.name])
 
 
